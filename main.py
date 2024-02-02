bb2b88da306d311d0384abe13d034b5958aac3aa
varde80 <h.lee0127@gmail.com>
2024-02-02 23:25:47 +0900

    first

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import torch
 from torch import optim, nn, utils, Tensor
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
+from torchvision import datasets
+import torch.utils.data as data
+from torch.utils.data import DataLoader
+
 import lightning as L
+from lightning.pytorch.callbacks import DeviceStatsMonitor
 
 # define any number of nn.Modules (or use your current ones)
 encoder = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
@@ -24,10 +29,33 @@ class LitAutoEncoder(L.LightningModule):
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
         x_hat = self.decoder(z)
-        loss = nn.functional.mse_loss(x_hat, x)
+        train_loss = nn.functional.mse_loss(x_hat, x)
         # Logging to TensorBoard (if installed) by default
-        self.log("train_loss", loss)
-        return loss
+        self.log("train_loss", train_loss)
+        return train_loss
+        
+    def validation_step(self, batch, batch_idx):
+        # this is the validation loop
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        val_loss = nn.functional.mse_loss(x_hat, x)
+        self.log("val_loss", val_loss,on_step=True, on_epoch=True, sync_dist=True)
+        return val_loss
+
+    def test_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        # it is independent of forward
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        test_loss = nn.functional.mse_loss(x_hat, x)
+        # Logging to TensorBoard (if installed) by default
+        self.log("test_loss", test_losson_step=True, on_epoch=True, sync_dist=True)
+        return test_loss
+        
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
@@ -38,12 +66,38 @@ class LitAutoEncoder(L.LightningModule):
 autoencoder = LitAutoEncoder(encoder, decoder)
 
 # setup data
-dataset = MNIST(os.getcwd(), download=True, transform=ToTensor())
-train_loader = utils.data.DataLoader(dataset)
+
+train_set = MNIST(root="MNIST", download=True, train=True, transform=ToTensor())
+
+# use 20% of training data for validation
+train_set_size = int(len(train_set) * 0.8)
+valid_set_size = len(train_set) - train_set_size
+
+# split the train set into two
+seed = torch.Generator().manual_seed(42)
+train_set, valid_set = data.random_split(train_set, [train_set_size, valid_set_size], generator=seed)
+test_set = MNIST(root="MNIST", download=True, train=False, transform=ToTensor())
+
+train_loader = DataLoader(train_set, batch_size=64)
+valid_loader = DataLoader(valid_set, batch_size=64)
+test_loader = DataLoader(test_set, batch_size=32)
+
 
 # train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
-trainer = L.Trainer(limit_train_batches=100, max_epochs=1)
-trainer.fit(model=autoencoder, train_dataloaders=train_loader)
+trainer = L.Trainer(
+    accelerator="mps", #gpu
+    devices=1,
+    # strategy="ddp",
+    # precision=16,
+    limit_train_batches=100,
+    max_epochs=10,
+    #profiler="simple",
+    callbacks=[DeviceStatsMonitor()],
+    default_root_dir="ckpt/",
+)
+
+trainer.fit(model=autoencoder, train_dataloaders=train_loader,val_dataloaders=valid_loader)
+trainer.test(model=autoencoder, dataloaders=test_loader)
 
 # load checkpoint
 checkpoint = "./lightning_logs/version_0/checkpoints/epoch=0-step=100.ckpt"
